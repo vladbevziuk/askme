82950bcc69bc74cee8cc37de7b6d7ed4e26f5951
vladbevziuk <vladbevziuk71@gmail.com>
2023-07-01 16:55:50 +0200

    continue configure controller, add new views

--- db/schema.rb
+++ db/schema.rb
@@ -19,12 +19,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_30_190658) do
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "nickname"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
